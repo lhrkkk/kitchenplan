cfa25048d4a1914581eb7c2667de94a744372ec8
lu haorui <luhaorui@gmail.com>
2014-08-23 17:48:00 +0800

    1

---
+++ test/untitled.rb
@@ -0,0 +1,42 @@
+RUBY_VERSION  # => "2.0.0"
+
+
+
+ping front
+PING front (210.45.66.91): 56 data bytes
+64 bytes from 210.45.66.91: icmp_seq=0 ttl=62 time=3.294 ms
+64 bytes from 210.45.66.91: icmp_seq=1 ttl=62 time=0.977 ms
+64 bytes from 210.45.66.91: icmp_seq=2 ttl=62 time=0.734 ms
+64 bytes from 210.45.66.91: icmp_seq=3 ttl=62 time=0.937 ms
+64 bytes from 210.45.66.91: icmp_seq=4 ttl=62 time=1.319 ms
+64 bytes from 210.45.66.91: icmp_seq=5 ttl=62 time=0.902 ms
+64 bytes from 210.45.66.91: icmp_seq=6 ttl=62 time=0.727 ms
+64 bytes from 210.45.66.91: icmp_seq=7 ttl=62 time=1.129 ms
+64 bytes from 210.45.66.91: icmp_seq=8 ttl=62 time=0.884 ms
+64 bytes from 210.45.66.91: icmp_seq=9 ttl=62 time=0.737 ms
+64 bytes from 210.45.66.91: icmp_seq=10 ttl=62 time=0.982 ms
+64 bytes from 210.45.66.91: icmp_seq=11 ttl=62 time=0.903 ms
+64 bytes from 210.45.66.91: icmp_seq=12 ttl=62 time=0.765 ms
+64 bytes from 210.45.66.91: icm
+
+
+
+p_seq=13 ttl=62 time=1.098 ms
+64 bytes from 210.45.66.91: icmp_seq=14 ttl=62 time=1.063 ms
+64 bytes from 210.45.66.91: icmp_seq=15 ttl=62 time=0.986 ms
+64 bytes from 210.45.66.91: icmp_seq=16 ttl=62 time=1.096 ms
+64 bytes from 210.45.66.91: icmp_seq=17 ttl=62 time=1.067 ms
+64 bytes from 210.45.66.91: icmp_seq=18 ttl=62 time=1.562 ms
+64 bytes from 210.45.66.91: icmp_seq=19 ttl=62 time=0.730 ms
+64 bytes from 210.45.66.91: icmp_seq=20 ttl=62 time=0.718 ms
+64 bytes from 210.45.66.91: icmp_seq=21 ttl=62 time=0.958 ms
+64 bytes from 210.45.66.91: icmp_seq=22 ttl=62 time=0.768 ms
+64 bytes from 210.45.66.91: icmp_seq=23 ttl=62 time=1.148 ms
+64 bytes from 210.45.66.91: icmp_seq=24 ttl=62 time=0.768 ms
+64 bytes from 210.45.66.91: icmp_seq=25 ttl=62 time=0.927 ms
+64 bytes from 210.45.66.91: icmp_seq=26 ttl=62 time=1.080 ms
+
+--- front ping statistics ---
+27 packets transmitted, 27 packets received, 0.0% packet loss
+round-trip min/avg/max/stddev = 0.718/1.047/3.294/0.482 ms
+
